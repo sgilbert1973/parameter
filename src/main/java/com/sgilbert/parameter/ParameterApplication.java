05e5ee417c2c95c5b38683924ec907ced1f049e9
Sylvain Gilbert <sylvain.gilbert@lacapitale.com>
2018-09-19 10:17:00 -0400

    Sprint Initializer

---
+++ src/main/java/com/sgilbert/parameter/ParameterApplication.java
@@ -0,0 +1,12 @@
+package com.sgilbert.parameter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParameterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParameterApplication.class, args);
+	}
+}
